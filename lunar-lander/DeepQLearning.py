803a6d9b4d009c158dd7864bab3cf748c72348dc
cemmanuelsr <caioemmanuelsr@gmail.com>
2022-05-23 07:58:12 -0300

    training agent with max steps 2500

--- lunar-lander/DeepQLearning.py
+++ lunar-lander/DeepQLearning.py
@@ -60,7 +60,7 @@ class DeepQLearning:
             state = np.reshape(state, (1, self.env.observation_space.shape[0]))
             score = 0
             #terminal = False
-            max_steps = 500 
+            max_steps = 2500 
             # no caso do lunarland, ele pode ficar em movimentos infinitos
             # sem alcancar um estado terminal. este eh um contador para evitar isto
             # de qualquer forma, quando terminal = True entao o episodio termina.
